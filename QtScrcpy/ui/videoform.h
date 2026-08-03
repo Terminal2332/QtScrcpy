74b1a78fc9c65b2a13a9162d2757aa4d44a16dd3
Terminal4233 <2771233274@qq.com>
2026-08-03 12:35:13 +0800

    Revert "fix: release pending steer wheel/touch states when window loses focus"
    This reverts commit 79c473438191007944ca276cc6f063c37392f65b.

--- QtScrcpy/ui/videoform.h
+++ QtScrcpy/ui/videoform.h
@@ -57,7 +57,6 @@ protected:
     void wheelEvent(QWheelEvent *event) override;
     void keyPressEvent(QKeyEvent *event) override;
     void keyReleaseEvent(QKeyEvent *event) override;
-    void changeEvent(QEvent *event) override;
 
     void paintEvent(QPaintEvent *) override;
     void showEvent(QShowEvent *event) override;
