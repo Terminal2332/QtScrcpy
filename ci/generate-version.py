5a01303de5f28499876440bcfd97f919849703a5
Terminal4233 <2771233274@qq.com>
2026-08-03 12:23:38 +0800

    ci: fallback version string when no git tag available in generate-version.py

--- ci/generate-version.py
+++ ci/generate-version.py
@@ -2,17 +2,21 @@ import sys
 import os
 
 if __name__ == '__main__':
+    # fallback: CI checkout may not fetch tags, then git describe fails
+    version = '3.3.1'
     p = os.popen('git rev-list --tags --max-count=1')
     commit = p.read()
     p.close()
 
-    p = os.popen('git describe --tags ' + commit)
-    tag = p.read()
-    p.close()
+    if commit:
+        p = os.popen('git describe --tags ' + commit)
+        tag = p.read()
+        p.close()
+        if tag:
+            version = str(tag[1:])
 
-    # print('get tag:', tag)
+    # print('get version:', version)
 
-    version = str(tag[1:])
     version_file = os.path.abspath(os.path.join(os.path.dirname(__file__), "../QtScrcpy/appversion"))
     file=open(version_file, 'w')
     file.write(version)
